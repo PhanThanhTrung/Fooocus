e0b2b5b590957b09243bb6dca4c745c44adfacbc
PhanThanhTrung <trung99nb@gmail.com>
2023-10-04 12:50:50 +0700

    add: UpScaler model instead of using uncontroled pipeline

--- modules/upscaler.py
+++ modules/upscaler.py
@@ -1,25 +1,52 @@
 import os
-import torch
-
-from comfy_extras.chainner_models.architecture.RRDB import RRDBNet as ESRGAN
-from comfy_extras.nodes_upscale_model import ImageUpscaleWithModel
 from collections import OrderedDict
-from modules.path import upscale_models_path
+from typing import Tuple
 
-model_filename = os.path.join(upscale_models_path, 'fooocus_upscaler_s409985e5.bin')
-opImageUpscaleWithModel = ImageUpscaleWithModel()
-model = None
+import torch
+from torch.cuda import OutOfMemoryError as OOM_EXCEPTION
+
+import comfy.utils as comfyutils
+from comfy_extras.chainner_models.architecture.RRDB import RRDBNet
 
 
-def perform_upscale(img):
-    global model
-    if model is None:
-        sd = torch.load(model_filename)
+class UpScaler():
+    def __init__(self, model_path: str, device: str = "cpu") -> None:
+        self.model = self._load_model(model_path=model_path, device=device)
+        self.device = device
+
+    def _load_model(self, model_path: str, device: str) -> RRDBNet:
+        assert os.path.exists(model_path), "Model doesn't exist!"
+        sd = torch.load(model_path)
         sdo = OrderedDict()
         for k, v in sd.items():
             sdo[k.replace('residual_block_', 'RDB')] = v
-        del sd
-        model = ESRGAN(sdo)
-        model.cpu()
+        model = RRDBNet(sdo)
+        model.to(device)
         model.eval()
-    return opImageUpscaleWithModel.upscale(model, img)[0]
+        return model
+
+    def perform_upscale(self, image: torch.Tensor) -> Tuple:
+        in_img = image.movedim(-1, -3).to(self.device)
+
+        tile = 512
+        overlap = 32
+
+        oom = True
+        s = None
+        while oom:
+            try:
+                steps = in_img.shape[0] * comfyutils.get_tiled_scale_steps(
+                    in_img.shape[3], in_img.shape[2], tile_x=tile, tile_y=tile, overlap=overlap)
+                pbar = comfyutils.ProgressBar(steps)
+                s = comfyutils.tiled_scale(in_img, lambda a: self.model(
+                    a), tile_x=tile, tile_y=tile, overlap=overlap, upscale_amount=self.model.scale, pbar=pbar)
+                oom = False
+            except OOM_EXCEPTION as e:
+                tile //= 2
+                if tile < 128:
+                    raise e
+        if s is not None:
+            s = torch.clamp(s.movedim(-3, -1), min=0, max=1.0)
+            return (s,)
+        else:
+            return (None,)
